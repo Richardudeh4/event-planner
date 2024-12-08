44185cc49426eff745b09d6a1340cd70c1eedc39
richardudeh4 <richardudeh03@gmail.com>
2024-12-08 15:04:16 +0100

    added the workspace switcher

--- features/workspaces/server/route.ts
+++ features/workspaces/server/route.ts
@@ -2,27 +2,52 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { createWorkspaceSchema } from "../schema";
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { DATABASE_ID, WORKSPACES_ID } from "@/app/config";
+import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/app/config";
 import { ID } from "node-appwrite";
 
 const app = new Hono()
+.get("/",sessionMiddleware, async(c) => {
+    const databases = c.get("databases");
+    const workspaces = await databases.listDocuments(
+        DATABASE_ID,
+        WORKSPACES_ID,
+    );
+    return c.json({data:workspaces})
+})
 .post(
     "/",
-    zValidator("json", createWorkspaceSchema),
+    zValidator("form", createWorkspaceSchema),
     sessionMiddleware,
     async (c) => {
         const databases = c.get("databases");
+        const storage = c.get("storage");
         const user = c.get("user");
-        const {name, image} = c.req.valid("json");
+        const {name, image} = c.req.valid("form");
 
+        let uploadImageUrl: string | undefined;
+        if(image instanceof File){
+            const file = await storage.createFile(
+                IMAGES_BUCKET_ID,
+                ID.unique(),
+                image,
+            );
+            const arrayBuffer = await storage.getFilePreview(
+                IMAGES_BUCKET_ID,
+                file.$id,
+            );
+            uploadImageUrl = `data:image/png;base64, ${Buffer.from(arrayBuffer).toString("base64")}`
+        }
         const workspace = await databases.createDocument(
             DATABASE_ID,
+
             WORKSPACES_ID,
             ID.unique(),
             {
                 name,
                 userId: user.$id,
+                imageUrl: uploadImageUrl,
             },
+
         );
         return c.json({data: workspace});
     }
